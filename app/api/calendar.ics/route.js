7941f69d516a552a6b2d640e9edec3434e0589cc
ZacoFunKy <zactanji@gmail.com>
2025-11-19 21:24:20 +0100

    [Fix] Regex

--- app/api/calendar.ics/route.js
+++ app/api/calendar.ics/route.js
@@ -25,7 +25,7 @@ const CONFIG = {
   INITIAL_BACKOFF: 500,
   TIMEOUT: parseInt(process.env.API_TIMEOUT || '6000'),
   CACHE_TTL: parseInt(process.env.CACHE_TTL || '3600'), // 1 heure
-GROUP_REGEX: /^[a-zA-Z0-9-_\s\.\(\)]+$/,
+GROUP_REGEX: /^[a-zA-Z0-9\s\-\_\.\(\)\,]+$/
 };
 
 export const dynamic = 'force-dynamic';
